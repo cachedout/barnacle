78875ba449c9e707d3bb3959c182d4293c38bf31
Ch3LL <mwilhite@saltstack.com>
2016-09-14 13:19:50 -0600

    add script to automate build

---
+++ auto_build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ALL_DIRS=`find . -name Dockerfile -printf '%h\n' | sed "s|^\./||"`
+
+function usage()
+{
+        echo "=========USAGE========="
+        echo "$0 -o <os> -a <all os>"
+        echo "You can only use -o or -a not both"
+        echo "When specifying -o you need to simply specify the OS/dir name you want to build"
+        echo "Your options for os are the following:"
+        echo "${ALL_DIRS}"
+        echo "When specifying -a this will build all OSs in this repo"
+        echo '      ./build.sh  -a'
+        echo "When specifying -o <os> this will build just that OS in this repo"
+        echo '      ./build.sh  -o "centos5'
+        exit
+}
+
+while getopts "o:ah" opt; do
+  case $opt in
+    o)
+      OS=${OPTARG}
+      ;;
+    a)
+      ALL=True
+      ;;
+    h)
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+# function that finds all Dockerfile dirs and builds them
+function build_all(){
+    for i in ${ALL_DIRS};do
+        cd ${CURRENT_DIR}/$i;
+        echo "BUILDING ${i}-salt"
+        docker build -t ${i}-salt . &
+    done
+}
+
+# function that builds specified OS
+function build_os() {
+    specific_os=$1
+    cd ${CURRENT_DIR}/${specific_os}
+    echo "BUILDING ${specific_os}"
+    docker build -t ${specific_os}-salt .
+}
+
+
+if [ -z ${OS} ] && [ -z ${ALL} ]; then
+    usage
+fi
+[[ ! -z ${OS} ]] && build_os ${OS}
+[[ ! -z ${ALL} ]] && build_all
